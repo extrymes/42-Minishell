14fc5db500cea1447e75e722ffa13190dd49cf50
extrymes <65893656+extrymes@users.noreply.github.com>
2024-09-04 20:37:14 +0200

    feat(libft): add `ft_isspace()`, `ft_strcmp()` and `ft_strstr()` functions

---
+++ libft/ft_strstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 14:00:26 by sabras            #+#    #+#             */
+/*   Updated: 2024/09/02 14:03:04 by sabras           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	if (!to_find[0])
+		return (str);
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (str[i + j] == to_find[j])
+		{
+			if (!to_find[j + 1])
+				return (&str[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
