a24d3cacf9db60f5b56f4a9e87faba52ca46c941
extrymes <65893656+extrymes@users.noreply.github.com>
2024-09-07 01:51:46 +0200

    fix(parsing): make sure the prompt is like bash

--- parsing/prompt.c
+++ parsing/prompt.c
@@ -6,29 +6,30 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 10:28:37 by sabras            #+#    #+#             */
-/*   Updated: 2024/08/31 11:02:57 by sabras           ###   ########.fr       */
+/*   Updated: 2024/09/06 15:59:54 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static char	*handle_home_pwd(t_data *data, int *allocated);
-static char	*pwd_join(char *s1, char *s2, int to_free);
 
 char	*create_prompt(t_data *data)
 {
 	char	*prompt;
+	char	*user;
 	char	*pwd;
 	int		allocated;
 
 	allocated = 0;
+	user = data->user;
 	pwd = handle_home_pwd(data, &allocated);
-	prompt = pwd_join(pwd_join(pwd_join(data->user,
-					"@minishell:", -1), pwd, 0), "$ ", 0);
+	prompt = strjoin_free(strjoin_free(strjoin_free(CYAN "", ft_strjoin(user,
+						"@minishell:"), 1), pwd, 0), "$ " RESET, 0);
 	if (allocated)
 		free(pwd);
 	if (!prompt)
-		return (throw_error("prompt create failure", data), NULL);
+		return (throw_error("malloc failure", data), NULL);
 	return (prompt);
 }
 
@@ -39,33 +40,21 @@ static char	*handle_home_pwd(t_data *data, int *allocated)
 	int		home_len;
 
 	home = ft_getenv("HOME", data->env);
-	pwd = data->pwd;
+	pwd = ft_getenv("PWD", data->env);
+	if (!pwd)
+		pwd = data->pwd;
 	home_len = ft_strlen(home);
-	if (pwd && home_len > 0)
+	if (home_len > 0)
 	{
+		if (!ft_strcmp(home, pwd) && home_len > 1)
+			return ("~");
 		if (!ft_strncmp(home, pwd, home_len) && pwd[home_len] == '/')
 		{
 			pwd = ft_strjoin("~", pwd + home_len);
 			if (!pwd)
-				return (throw_error("pwd handling error", data), NULL);
+				return (throw_error("malloc error", data), NULL);
 			*allocated = 1;
 		}
 	}
 	return (pwd);
-}
-
-static char	*pwd_join(char *s1, char *s2, int to_free)
-{
-	char	*joined;
-
-	joined = NULL;
-	if (s1 && s2)
-		joined = strjoin_free(s1, s2, to_free);
-	else if (!s1 && s2)
-		joined = strjoin_free("", s2, to_free);
-	else if (!s2 && s1)
-		joined = strjoin_free(s1, "", to_free);
-	if (!joined)
-		return (NULL);
-	return (joined);
 }
