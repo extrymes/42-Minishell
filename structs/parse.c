89fbbb5bb2947420050e21fc15ac827185d179ec
extrymes <65893656+extrymes@users.noreply.github.com>
2024-09-10 22:55:37 +0200

    chore(structs): remove unnecessary `clear_parse()` function

--- structs/parse.c
+++ structs/parse.c
@@ -6,7 +6,7 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 04:19:57 by sabras            #+#    #+#             */
-/*   Updated: 2024/09/08 04:57:30 by sabras           ###   ########.fr       */
+/*   Updated: 2024/09/10 22:22:12 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,4 @@ t_parse	init_parse(t_data *data, char *input)
 	p.i = -1;
 	p.j = 0;
 	return (p);
-}
-
-void	clear_parse(t_parse *p)
-{
-	if (p->input)
-	{
-		free(p->input);
-		p->input = NULL;
-	}
-	if (p->parsed)
-	{
-		free(p->parsed);
-		p->parsed = NULL;
-	}
 }
