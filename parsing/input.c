fc5d1a81e45f61b8ff07cdeb60a07c233becd7ea
extrymes <65893656+extrymes@users.noreply.github.com>
2024-09-12 12:09:41 +0200

    feat(parsing): handle heredoc

--- parsing/input.c
+++ parsing/input.c
@@ -6,19 +6,21 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 19:56:48 by sabras            #+#    #+#             */
-/*   Updated: 2024/09/11 16:08:04 by sabras           ###   ########.fr       */
+/*   Updated: 2024/09/12 12:06:52 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static void	find_cmd(t_data *data, t_cmd *cmd, t_token *token);
-static int	find_file(t_data *data, t_cmd *cmd, t_token *token);
+static int	find_file(t_data *data, t_cmd *cmd, t_token *token, int i);
+static void	handle_heredoc(t_data *data, t_cmd *cmd, t_token *token, int i);
 
 void	parse_input(t_data *data, t_entry *entry)
 {
 	t_token	*token;
 	t_cmd	*cmd;
+	int		i;
 
 	entry->input = handle_variables(data, entry->input);
 	tokenize_input(data, entry->input);
@@ -26,13 +28,14 @@ void	parse_input(t_data *data, t_entry *entry)
 		return ;
 	token = entry->token_lst;
 	cmd = init_cmd(data);
+	i = 0;
 	while (token)
 	{
 		if (!cmd->name)
 			find_cmd(data, cmd, token);
-		if (token->type == PIPE)
+		if (token->type == PIPE && ++i)
 			cmd = init_cmd(data);
-		if (find_file(data, cmd, token))
+		if (find_file(data, cmd, token, i))
 			token = token->next;
 		token = token->next;
 	}
@@ -61,10 +64,42 @@ static void	find_cmd(t_data *data, t_cmd *cmd, t_token *token)
 	return ;
 }
 
-static int	find_file(t_data *data, t_cmd *cmd, t_token *token)
+static int	find_file(t_data *data, t_cmd *cmd, t_token *token, int i)
 {
 	if (token->type < FILE_IN)
 		return (0);
-	add_file(data, cmd, token->next->content, token->type);
+	if (token->type == HERE_DOC)
+		handle_heredoc(data, cmd, token, i);
+	else
+		add_file(data, cmd, token->next->content, token->type);
 	return (1);
+}
+
+static void	handle_heredoc(t_data *data, t_cmd *cmd, t_token *token, int i)
+{
+	char	*filename;
+	char	*delimiter;
+	char	*line;
+	int		fd;
+
+	filename = strjoin_free("/tmp/heredoc", ft_itoa(i), 1);
+	if (!filename)
+		throw_error(data, "malloc failure");
+	delimiter = token->next->content;
+	fd = open(filename, O_TRUNC | O_CREAT | O_RDWR, 0644);
+	if (fd < 0)
+		throw_error(data, "open failure");
+	while (1)
+	{
+		line = readline(CYAN "> " RESET);
+		if (!line || !ft_strcmp(line, delimiter))
+			break ;
+		line = handle_variables(data, line);
+		ft_putstr_fd(line, fd);
+		ft_putstr_fd("\n", fd);
+		free(line);
+	}
+	close(fd);
+	signal(SIGINT, sigint_handler_nonl);
+	add_file(data, cmd, filename, token->type);
 }
