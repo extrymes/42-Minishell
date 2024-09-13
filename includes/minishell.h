a9b701a2c7995326c438281d0d356e55f22b368b
extrymes <65893656+extrymes@users.noreply.github.com>
2024-09-13 21:48:19 +0200

    chore: update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,19 +6,21 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 09:33:57 by sabras            #+#    #+#             */
-/*   Updated: 2024/09/08 22:32:05 by sabras           ###   ########.fr       */
+/*   Updated: 2024/09/13 21:46:41 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include "../libft/libft.h"
+# include "errors.h"
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include <sys/types.h>
 # include <sys/wait.h>
-// # include <sys/stat.h>
+# include <sys/stat.h>
+# include <signal.h>
 # include <fcntl.h>
 # define BUILTINS "cd echo env exit export pwd unset"
 
@@ -28,6 +30,7 @@ typedef struct s_entry	t_entry;
 typedef struct s_token	t_token;
 typedef struct s_cmd	t_cmd;
 typedef struct s_arg	t_arg;
+typedef struct s_file	t_file;
 typedef struct s_parse	t_parse;
 typedef struct s_pipex	t_pipex;
 
@@ -37,6 +40,9 @@ struct s_data
 	char	*home;
 	char	*pwd;
 	char	**env;
+	int		stdin_fd;
+	int		stdout_fd;
+	int		exit_code;
 	t_entry	*entry;
 };
 
@@ -52,6 +58,8 @@ struct s_entry
 struct s_token
 {
 	char	*content;
+	int		type;
+	int		has_quote;
 	t_token	*next;
 };
 
@@ -61,8 +69,8 @@ struct s_cmd
 	char	*path;
 	t_arg	*arg_lst;
 	int		arg_count;
-	int		infile;
-	int		outfile;
+	t_file	*file_lst;
+	char	*err;
 	t_cmd	*next;
 };
 
@@ -70,6 +78,13 @@ struct s_arg
 {
 	char	*data;
 	t_arg	*next;
+};
+
+struct s_file
+{
+	char	*name;
+	int		redir;
+	t_file	*next;
 };
 
 struct s_parse
@@ -86,18 +101,22 @@ struct s_pipex
 {
 	int		saved_stdin;
 	int		saved_stdout;
+	int		infile;
+	int		outfile;
 	pid_t	*pid;
 	int		here_doc;
 	int		tube[2];
 };
 
 // -- Enumerations --
-enum e_redir
+enum e_types
 {
-	FILE_IN = 1,
-	HERE_DOC = 2,
-	FILE_OUT = 3,
-	FILE_OUT_APP = 4
+	WORD,
+	PIPE,
+	FILE_IN,
+	HERE_DOC,
+	FILE_OUT,
+	FILE_OUT_APP
 };
 
 // --- Parsing ---
@@ -105,16 +124,18 @@ enum e_redir
 void	tokenize_input(t_data *data, char *input);
 
 // input.c
-void	parse_input(t_data *data, t_entry *entry);
+void	*parse_input(t_data *data, t_entry *entry);
 
-// input_utils.c
-char	*insert_value(t_data *data, t_parse *p);
-char	*insert_home(t_data *data, t_parse *p);
+// wildcards.c
+void	handle_wilcards(t_data *data, char *content);
+
+// variables.c
+char	*handle_variables(t_data *data, char *input, int heredoc);
 
 // command.c
 char	*get_cmd_name(t_data *data, char *path);
 char	*get_cmd_path(t_data *data, char *name);
-int		is_command(t_data *data, char *content);
+int		check_command(t_data *data, char *content, char **err);
 
 // prompt.c
 char	*create_prompt(t_data *data);
@@ -124,40 +145,42 @@ void	read_env(t_data *data, char **env);
 char	*ft_getenv(char *key, char **env);
 
 // syntax.c
-int		check_syntax(t_token *token_lst);
-int		check_redir(char *s);
+int		check_syntax(t_data *data, t_token *token_lst);
 
 // metachars.c
 char	toggle_quote(char c, char quote);
 int		is_opt(char c);
 int		is_quote(char c);
-int		is_valid_key(char c);
-int		is_valid_tilde(char c);
+int		check_key(char c);
+int		check_tilde(char c);
 
 // --- Execution ---
-void	pipex(t_data *data);
+void	exec_data(t_data *data);
 void	one_cmd(t_data *data, t_pipex *pipex);
 void	ft_exec(t_cmd *cmd, t_data *data);
-void	set_file(t_pipex *pipex);
+int		set_file(t_file *file, t_pipex *pipex, t_data *data);
 t_cmd	*get_cmd_by_id(t_cmd *cmd_lst, int id);
-void	error_exec(t_data *data);
+void	error_exec(t_data *data, char *str);
 void	ft_close(int fd);
-void	reset_std(t_pipex *pipex);
+void	reset_std(t_data *data);
+void	stop_process(t_data *data, t_pipex *pipex);
 
 // builtins
 void	builtins(t_cmd *cmd, t_data *data, int in_fork);
-int		check_exit(t_data *data);
-void	ft_echo(t_cmd *cmd);
+void	ft_echo(t_data *data, t_cmd *cmd);
 void	ft_cd(t_cmd *cmd, t_data *data);
 void	ft_env(t_data *data);
 void	ft_pwd(t_data *data);
-void	ft_exit(t_data *data);
+void	ft_exit(t_cmd *cmd, t_data *data);
 
 void	ft_export(t_cmd *cmd, t_data *data);
+void	print_x(char *str, int a);
+char	*copy_to(char *env, char c, char start);
 int		is_var_exists(char **env, char *str);
 char	**copy_tab(t_data *data);
+int		check_arg(char *str);
 
-void	ft_unset(t_cmd *cmd, t_data *data);
+void	*ft_unset(t_cmd *cmd, t_data *data);
 
 // --- Structs ---
 // data.c
@@ -169,12 +192,15 @@ t_entry	*init_entry(t_data *data);
 void	clear_entry(t_entry *entry);
 
 // token.c
-void	add_token(t_data *data, t_entry *entry, char *content);
+t_token	*init_token(t_data *data, char *content, int type, int has_quote);
+void	add_token(t_entry *entry, t_token *token);
+int		get_token_type(char *content);
 void	clear_token_lst(t_token *token_lst);
 
 // cmd.c
-t_cmd	*init_cmd(t_data *data, char *content);
+t_cmd	*init_cmd(t_data *data);
 void	add_cmd(t_entry *entry, t_cmd *cmd);
+void	set_cmd_data(t_data *data, t_cmd *cmd, char *content);
 void	clear_cmd_lst(t_cmd *cmd_lst);
 
 // arg.c
@@ -182,16 +208,31 @@ t_arg	*init_arg(t_data *data, char *content);
 void	add_arg(t_data *data, t_cmd *cmd, char *content);
 void	clear_arg_lst(t_arg *arg_lst);
 
+// file.c
+t_file	*init_file(t_data *data, char *name, int redir);
+void	add_file(t_data *data, t_cmd *cmd, char *content, int redir);
+void	clear_file_lst(t_file *file_lst);
+
 // parse.c
 t_parse	init_parse(t_data *data, char *input);
-void	clear_parse(t_parse *parse);
 
 // --- Utils ---
+// signals.c
+void	handle_signals(int in_process);
+void	reset_signal_status(void);
+
+// events.c
+int		rl_event_handler(void);
+
 // memory.c
 char	*ft_realloc(char *s, size_t size);
 void	*ft_realloc2(void *ptr, size_t original_size, size_t new_size);
 char	*alloc_str(t_data *data, char *str);
 void	free_split(char **tab);
+
+// string.c
+char	*strjoin_free(char *s1, char *s2, int to_free);
+int		is_alpha(char *str);
 
 // strjoin_free.c
 char	*strjoin_free(char *s1, char *s2, int to_free);
@@ -200,8 +241,10 @@ char	*strjoin_free(char *s1, char *s2, int to_free);
 int		ft_strcmp(char *s1, char *s2);
 
 // error.c
-void	print_syntax_error(char *token);
-void	print_cmd_error(char *cmd, char *err, char *arg);
-void	throw_error(char *err, t_data *data);
+void	syntax_error(t_data *data, char *token);
+void	cmd_error(char *cmd, char *arg, char *err);
+void	heredoc_warning(char *delimiter);
+char	*get_error(t_data *data, char *content, char *err, int code);
+void	throw_error(t_data *data, char *err);
 
 #endif
